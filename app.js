746d3f1a1aa36f3edaafedadf1af605e22f70e1f
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-06 11:39:27 +0300

    Add initial schema and root query

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const port = 4000;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+  }),
+);
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}...`);
