72633b0f67b83207a9f618179ef30993e8757c69
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-06 16:03:55 +0300

    Add DB connection

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
+require('dotenv').config();
 
 const app = express();
 const port = 4000;
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to Database');
+});
 
 app.use(
   '/graphql',
